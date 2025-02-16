181ad856c472a8f2f7015838a9511c778ebcde9d
bonaxl015 <bonaxl015@gmail.com>
2025-02-16 23:14:35 +0800

    feat: root component UI development

---
+++ src/components/App.test.ts
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+
+import App from './App.vue';
+
+describe('App.vue', () => {
+  it('renders a message', () => {
+    const wrapper = mount(App);
+
+    expect(wrapper.text()).toContain('Hello Vue 3');
+  });
+});
---
+++ src/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <h1>Hello Vue 3</h1>
+    <h1>{{ count }}</h1>
+    <button @click="increment">increment</button>
+    <button @click="decrement">decrement</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const count = ref<number>(0);
+
+const increment = () => {
+  count.value = count.value + 1;
+};
+
+const decrement = () => {
+  count.value = count.value - 1;
+};
+</script>
+
+<style scoped>
+.app {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
