c874f512dfaed64fb4056d60711d89a584335f7c
bonaxl015 <bonaxl015@gmail.com>
2025-02-23 15:25:53 +0800

    feat(router): setup vue router and add pages

--- src/components/App/index.vue
+++ src/components/App/index.vue
@@ -1,31 +1,11 @@
 <template>
   <div class="app">
-    <h1 class="header">Hello Vue 3</h1>
-    <h1>{{ count }}</h1>
-    <button @click="increment">increment</button>
-    <button @click="decrement">decrement</button>
+    <RouterView />
   </div>
-  <ThemeSwitch />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-import ThemeSwitch from '../ThemeSwitch/index.vue';
-
-const count = ref<number>(0);
-
-const increment = () => {
-  count.value = count.value + 1;
-};
-
-const decrement = () => {
-  count.value = count.value - 1;
-};
+import { RouterView } from 'vue-router';
 </script>
 
-<style lang="scss" scoped>
-.header {
-  color: red;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/utils/routeList.ts
@@ -0,0 +1,27 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/home',
+    name: 'home',
+    strict: true,
+    sensitive: true,
+    component: () => import('@pages/Home/index.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    strict: true,
+    sensitive: true,
+    component: () => import('@pages/About/index.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    strict: true,
+    sensitive: true,
+    component: () => import('@pages/NotFound/index.vue')
+  }
+];
+
+export default routes;
