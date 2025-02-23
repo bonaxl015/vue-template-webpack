f1c794374d85b5cfa19cd03631e9f4d07f1ae65d
bonaxl015 <bonaxl015@gmail.com>
2025-02-23 12:43:28 +0800

    feat(vuetify): install and setup vuetify

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,16 @@
+import { jest } from '@jest/globals';
+
 global.console = {
   ...console,
   warn: jest.fn(),
   error: jest.fn()
 };
+
+jest.mock('vuetify', () => ({
+  useTheme: jest.fn(() => ({
+    global: {
+      current: { value: { dark: false } },
+      name: { value: 'light' },
+    },
+  })),
+}));
