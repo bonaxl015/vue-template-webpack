334c0421f0aeb6a7349a8d0caa6dfc861b0ba32d
bonaxl015 <bonaxl015@gmail.com>
2025-03-01 10:30:04 +0800

    feat(navigation): update header to support navigation

--- src/components/Header/index.test.ts
+++ src/components/Header/index.test.ts
@@ -1,5 +1,5 @@
 import { mount, VueWrapper } from '@vue/test-utils';
-import { createVuetify } from 'vuetify';
+import { createVuetify, useDisplay } from 'vuetify';
 
 import Header from './index.vue';
 
@@ -7,13 +7,35 @@ const vuetify = createVuetify();
 
 jest.mock('../ThemeSwitch/index.vue', () => ({
   name: 'ThemeSwitch',
-  template: '<div aria-label="theme-switch"></div>'
+  template: '<div data-test="theme-switch"></div>'
 }));
+
+jest.mock('../NavigationDrawer/index.vue', () => ({
+  name: 'NavigationDrawer',
+  template: '<div data-test="navigation-drawer"></div>'
+}));
+
+jest.mock('../NavigationButtons/index.vue', () => ({
+  name: 'NavigationButtons',
+  template: '<div data-test="navigation-buttons"></div>'
+}));
+
+jest.mock('vuetify', () => {
+  const originalModule = jest.requireActual('vuetify');
+
+  return {
+    ...originalModule,
+    useDisplay: jest.fn()
+  };
+});
 
 describe('Given Header.vue component', () => {
   let wrapper: VueWrapper;
 
   beforeEach(() => {
+    jest.resetModules();
+    (useDisplay as jest.Mock).mockReturnValue({ xs: false });
+
     wrapper = mount(
       {
         template: `<v-app><Header /></v-app>`,
@@ -29,9 +51,12 @@ describe('Given Header.vue component', () => {
 
   it('renders the Header component without any issues', () => {
     expect(wrapper.find('.v-app-bar').exists()).toBe(true);
-    expect(wrapper.find('.v-app-bar-nav-icon').exists()).toBe(true);
+    expect(wrapper.find('.v-app-bar-nav-icon').exists()).toBe(false);
     expect(wrapper.find('.v-app-bar-title').exists()).toBe(true);
     expect(wrapper.find('.v-app-bar-title').text()).toBe('Vue Template');
-    expect(wrapper.find('[aria-label="theme-switch"]').exists()).toBe(true);
+    expect(wrapper.find('[data-test="theme-switch"]').exists()).toBe(true);
+    expect(wrapper.find('[data-test="navigation-buttons"]').exists()).toBe(
+      true
+    );
   });
 });
