fb2a162153d5dc60c88143a7e90a3bb838145cff
bonaxl015 <bonaxl015@gmail.com>
2025-03-01 13:10:41 +0800

    feat(homepage): homepage UI development

---
+++ src/pages/Home/constants/checkboxList.ts
@@ -0,0 +1,50 @@
+export const libraryListLeft = [
+  {
+    key: '1',
+    label: 'Webpack',
+    value: 'webpack'
+  },
+  {
+    key: '2',
+    label: 'Vue',
+    value: 'vue'
+  },
+  {
+    key: '3',
+    label: 'Vuetify',
+    value: 'vuetify'
+  },
+  {
+    key: '4',
+    label: 'Typescript',
+    value: 'typescript'
+  },
+  {
+    key: '5',
+    label: 'Babel',
+    value: 'babel'
+  }
+];
+
+export const libraryListRight = [
+  {
+    key: '6',
+    label: 'EsLint',
+    value: 'eslint'
+  },
+  {
+    key: '7',
+    label: 'Jest',
+    value: 'jest'
+  },
+  {
+    key: '8',
+    label: 'Prettier',
+    value: 'prettier'
+  },
+  {
+    key: '9',
+    label: 'Vuetify',
+    value: 'vuetify'
+  }
+];
