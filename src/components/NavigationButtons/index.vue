6ce5528cc0ef81da98193075880a42009a942a81
bonaxl015 <bonaxl015@gmail.com>
2025-03-01 10:29:26 +0800

    feat(navigation): header navigation buttons

---
+++ src/components/NavigationButtons/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="nav-button-wrapper" aria-label="navigation-buttons">
+    <v-btn variant="text" to="/">Home</v-btn>
+    <v-btn variant="text" to="/about">About</v-btn>
+  </div>
+</template>
+
+<style lang="scss">
+.nav-button-wrapper {
+  display: inline-flex;
+  gap: 0.25rem;
+  margin-right: 1.5rem;
+}
+</style>
