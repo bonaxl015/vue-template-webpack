e2280d257e5dd02961bb08d5812e2c4ecd156667
bonaxl015 <bonaxl015@gmail.com>
2025-03-03 13:42:58 +0800

    chore: updated typescript and jest setup to support images

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,7 @@ module.exports = {
   ],
   coverageReporters: ["text", "json-summary"],
   moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mocks/fileMock.js',
     '\\.(css|less|scss|sss|styl)$': 'jest-css-modules',
     '^@assets/(.*)$': getPathMapper('assets'),
     '^@components/(.*)$': getPathMapper('components'),
